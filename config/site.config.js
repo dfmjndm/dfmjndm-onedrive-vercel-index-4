da2e543f2fc425b9d3fc7e147256ea4358559604
dfmjndm <57401901+dfmjndm@users.noreply.github.com>
2022-02-22 15:10:38 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "My OneDrive",
+  title: "教育版onedrive快速入口",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/',
