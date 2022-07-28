6ed8647b807e9770c070f6ce20919829cae97caa
dfmjndm <57401901+dfmjndm@users.noreply.github.com>
2022-07-29 01:06:58 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "教育版onedrive快速入口",
+  title: "教育版onedrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/',
