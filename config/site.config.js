f9cc92b7ff0e44ca81b409bb8eb6a68f1b4bbf9c
dfmjndm <57401901+dfmjndm@users.noreply.github.com>
2022-07-30 21:27:51 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // Prefix for KV Storage
-  kvPrefix: process.env.KV_PREFIX || '',
+  kvPrefix: process.env.KV_PREFIX || '1111',
 
   // The name of your website. Present alongside your icon.
   title: "教育版onedrive",
